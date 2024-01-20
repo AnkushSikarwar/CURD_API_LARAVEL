48fe3b4225388d3acf3e3d7008da2d7ec6fbac19
AnkushSikarwar <singhankush2308@gmail.com>
2024-01-21 01:18:16 +0530

    Setup for Migration of reviews and products

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,11 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use App\Models\Review;
 class Product extends Model
 {
     use HasFactory;
     protected $fillablle =[
         'name','description','price','discount'
     ];
+    public function reviews(){
+        return $this->hasMany(Review::class);
+    }
 }
--- app/Models/Review.php
+++ app/Models/Review.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Models;
-
+use App\Models\Product;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -11,4 +11,7 @@ class Review extends Model
     protected $fillablle =[
         'customer','review','star'
     ];
+    public function product(){
+        return $this->belongsTo(Product::class);
+    }
 }
